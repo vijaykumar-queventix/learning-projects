d0f111dc5afad43bd736e0a9c89277686c1286b2
Vijay Kumar <vijay.kumar@quventix.com>
2021-02-11 16:05:53 +0530

    generating html

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const port = 3000;
 
 // creating server
 http.createServer((req, res)=>{
-    res.writeHead(200, {'content-type': 'text/plain'});
-    res.end('this is first program');
+    const html = buildHtml(req);
+
+    res.writeHead(200, {'content-type': 'text/html'});
+    res.end(html);
 }).listen(port, ()=>{
     console.log(`server running on ${port}`);
-})
+});
+
+function buildHtml(req){
+    var header ="";
+    var body = '<h1>Node js generating first html</h1>';
+    return '<!DOCTYPE html>' + '<html><header>' + header + '</header><body>' + body + '</body></html>'
+}
