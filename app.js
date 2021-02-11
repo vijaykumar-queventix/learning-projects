bec7d878c0200f6542ecb6cda3d350ff5d54cc39
Vijay Kumar <vijay.kumar@quventix.com>
2021-02-11 17:28:17 +0530

    http get request

--- app.js
+++ app.js
@@ -1,27 +1,8 @@
 var http = require('http');
-var uhtml = 
-'<html><head><title>Post Example</title></head>' +
-'<body>' +
-'<p>Input your name and Address</p>'+
-'<form method="post">' +
-'Name : <input name="name" size=20><br>' +
-'Address : <input name="address" size=50><br>' +
-'<input type="submit">' +
-'</form>' +
-'</body></html>';
 
-http.createServer(function (req, res) {
-var body = "";
-
-// reading from body data
-req.on('data', function (chunk) {
-body += chunk;
+var url = "http://www.google.com";
+http.get(url, (res)=>{
+    console.log(`Got response  : ${res.statusCode}`);
+}).on('error', (e)=>{
+    console.log(`Got error : ${e.message}`);
 });
-
-
-req.on('end', function () {
-console.log('POSTed: ' + body);
-res.writeHead(200);
-res.end(uhtml);
-});
-}).listen(3000);
