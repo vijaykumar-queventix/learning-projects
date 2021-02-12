37e2ef79dc6eb8982914099e8f2d7dae4ab55df2
Vijay Kumar <vijay.kumar@quventix.com>
2021-02-12 17:40:25 +0530

    steams-piping the stream

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-// writing to a stream
+// Piping the stream
 
 const fs = require('fs');
-const data = "It is simply leaning";
 
-// creating writeable stream
-let writereStream = fs.createWriteStream('output.txt');         // creating empty output.txt file
+// creating read stream
+const readerStream = fs.createReadStream('input.txt');
 
-// write the data to stream with encoding to be utf-8
-writereStream.write(data, 'utf-8');
-
-// handling finish and error event
-writereStream.on('finish', ()=>{
-    console.log(`Task is completed :
-content of data written in output.txt`)
-})
+// creating writing stream
+const writingStream = fs.createWriteStream('output.txt');
 
 
-writereStream.on('error', (error)=>{
-    console.log(error);
-});
-
-// mark the end of file
-writereStream.end();
-
-
+// pipe read the stream and write to stream
+// read input.txt and write content of imput.txt to output.txt
+readerStream.pipe(writingStream);
 console.log('program ended');
