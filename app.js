1ca10c9e9a6b639ee64ebea82700c258e0875421
Vijay Kumar <vijay.kumar@quventix.com>
2021-02-12 17:00:45 +0530

    streams-reading from stream

--- app.js
+++ app.js
@@ -1,55 +1,29 @@
-// creates an empty buffer of lenth 15
-var buff = Buffer.alloc(15)
-console.log(buff);
+// reading from a stream
 
-// convert string into binary data
-const b = Buffer.from('abc');
-console.log(b);
+const fs = require('fs');
+let data = "";
 
-const b1 = Buffer.from('abcd');
-console.log(b1);
+// creating reading stream
+let readerStream = fs.createReadStream('test.txt');
+//console.log(readerStream);
 
-// creates buffer of 10 byte
-const c = Buffer.allocUnsafe(10);
-console.log(c);
+// set encoding to be utf-8
+readerStream.setEncoding('utf-8');
 
+// handle stream events data, end and error 
 
-// it compares two buffers and return
-// 0 if equal
-// 1 if first buffer is higher than second buffer
-// -1 if first buffer is lower than second buffer
-const b3 = Buffer.compare(b, b1);
-console.log(b3);
-
-// joins array buffer in single buffer
-const arr = [b, b1]
-const combinedbuff = Buffer.concat(arr);
-console.log(combinedbuff);
-
-console.log('<-------------------------------------------->');
-
-// data encoding
-let originalString = 'this is original string';
-let buffobj = Buffer.from(originalString, 'utf-8');
-console.log(buffobj);
-
-let base64String = buffobj.toString('base64');
-console.log(`This is encoded base64 string is : ${base64String}`);
-
-let utf8String = buffobj.toString('utf-8');
-console.log(`This is encoded utf-8 string : ${utf8String}`);
-
-let hexString = buffobj.toString('hex');
-console.log(`This is encoding hex string is : ${hexString}`);
+readerStream.on('data', (chunk)=>{
+    data+=chunk;
+});
 
 
+readerStream.on('end', ()=>{
+    console.log(data);
+});
 
-console.log('<-------------------------------------------->');
-
-// decoding hex string to utf-8 string
-let hexbuffObj = Buffer.from(hexString, 'hex');           // converting hexstring to buffer object
-let decodedString = hexbuffObj.toString('utf-8');       // decoding
-
-console.log(`This is decoding hex string to utf-8 string : ${decodedString}`);
+readerStream.on('error', (error)=>{
+    console.log(`ERROR : ${error}`)
+});
 
 
+console.log('Program end');
