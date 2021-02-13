9378104d4fafe2f1437be259545b5cfcce2eaf30
Vijay Kumar <vijay.kumar@quventix.com>
2021-02-13 18:09:24 +0530

    Classes in javascript

--- app.js
+++ app.js
@@ -1,39 +1,49 @@
-// async/awit
-// task : api student
-// 1 : after 2sec display roll no.
-// 2 : after 2sec display name and age
+// classes in javascript
 
+// Defing class
+class student {
+    constructor (name, age , Class){            // constructor of class
+        this.name = name,
+        this.age = age,
+        this.Class = Class
+    }
+    // instence method
+    bioData(){
+        console.log(`name : ${this.name}, age : ${this.age}, class : ${this.Class}`);
+    }
 
-//producing promise
-const pobj1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        let roll_no = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        resolve(roll_no);
-        reject("Error :");
-    }, 2000);
-});
-
-// producing promise
-const getBioData = (indexData) => {
-    return new Promise((resolve, reject) => {
-        setTimeout((indexData) => {
-            let bioData = {
-                name: "demo",
-                age: 15
-            }
-            resolve(`My rollno is ${indexData}, My name is ${bioData.name} and I am ${bioData.age} years old.`)
-        }, 2000, indexData);
-    });
+    static accessBioData(){                         // static mathod cannot be accessed by obj created by student class
+        console.log(`name is : ${this.name}, age is : ${this.age}, class is : ${this.Class}`);
+    }
 }
 
+// creating object (instance of class)
+
+var obj1 = new student('demo',15,'bca');
+console.log(obj1);
+obj1.bioData();
+//obj1.accessBioData();
 
 
-async function getData (){
-    var rollnodata = await pobj1;
-    var getNameData = await getBioData(rollnodata[2]);
-    console.log(rollnodata);
-    console.log(getNameData )
+// creating another object with student class
+let obj2 = new student('demo2',45, 'Mcs');
+obj2.bioData();
+
+// inherting student class properties
+class player extends student{
+    constructor(name, age , Class,hobbies, city){
+        super(name, age , Class);           // inheritng constructor properties of student constructor
+        this.hobbies = hobbies,
+        this.city = city
+    }
+
+    // instence method
+    about(){
+        console.log(` hey my name is ${this.name} and my game is ${this.hobbies}.`);
+    }
 }
 
-getData();
-
+let obj3 = new player('demo',43,'abc','cricket','mohali');
+obj3.bioData();
+obj3.about();
+//obj3.accessBioData();
