f3f00a184c547b9fe9a63d76317022c70cf6c468
Vijay Kumar <vijay.kumar@quventix.com>
2021-02-13 11:26:28 +0530

    Feautes of ES6-Template string methods

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const example=()=>{
     var name = 'demo'; // can be accessed to nearest function;
+    var name1 = 'demo1';
           
     console.log("The string in variable name is : " + name );       // without template string
-    console.log(`The string in variable name is : ${name}`);     // template string example
+    console.log(`The string in variable name is : ${name}`)         // template string example
+
+    // template string methods
+    console.log(`The string in variable name is : ${name}`.startsWith('d'));     // if the variable string start with 'd' returns true otherwise false
+    console.log(`The string in variable name is : ${name}`.endsWith('o'));      // if the variable string ends with 'o' returns true otherwise false
+    console.log(`The string in variable name is : ${name}`.includes('em'));       // if the variable string includes  'e' returns true otherwise false
+    console.log(`The string in variable name is : ${name} `.repeat(5));           // it will repeat the string 5 times   
+    //  console.log(name).startsWith('d');
 
     console.log(name);
 
