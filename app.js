c4adcefb476f70e8939d5d2c9b2f2bf40b0409ae
Vijay Kumar <vijay.kumar@quventix.com>
2021-02-13 16:00:19 +0530

    promises

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-// ES7 (ECMAScript 2016)
+// promises
+// task : api student
+// 1 : after 2sec display roll no.
+//  : after 2sec display name and age
 
-let Names = ['demo1','demo2','demo3','demo4','demo5','demo6']
 
-console.log(Names.includes('demo3'));           // return true if demo3 exist in names array
+// producing promise
+const pobj1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let roll_no = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        resolve(roll_no);
+        reject("Error :");
+    }, 2000);
+});
 
-// Exponentiation operator
 
-let a = 2**5;               // this means 2*2*2*2*2
-console.log(a);
+const getBioData = (indexData) => {
+    return new Promise((resolve, reject) => {
+        setTimeout((indexData) => {
+            let bioData = {
+                name: "demo",
+                age: 15
+            }
+            resolve(`My rollno is ${indexData}, My name is ${bioData.name} and I am ${bioData.age} years old.`)
+        }, 2000, indexData);
+    });
+}
 
-// Before ES7
-let b = Math.pow(2,5);
-console.log(b);
+// consuming promises 
+pobj1.then((rollno) => {                         // to get what is in resolve
+    console.log(rollno);
+    return getBioData(rollno[2])
+}).then((data)=>{
+    console.log(data )
+}).catch((error) => {
+    console.log(error);
+})
+
+
+
