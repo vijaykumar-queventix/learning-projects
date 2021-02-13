13ba5e608c1c61afec6e071753aad9bcaf25273b
Vijay Kumar <vijay.kumar@quventix.com>
2021-02-13 10:52:24 +0530

    Features of ES6-Let and Const

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-// decompressing the stream
+const example=()=>{
+    var name = 'demo'; // can be accessed to nearest function;
+    name = 'demoChanged';
+    var name = 'created again with var';        // vARiable declared with var keyword can be redeclare
 
-const fs = require('fs');
-const zlib = require('zlib');
+    console.log(name);
 
-// decompress the input.txt.gz file to input.txt
+    let name1 = 'demo2';
+    name1 = 'demo2Changed';
+    //let name1 = "Cannot create again with let keyword";         // vARiable declared with let keyword cannot be redeclare
 
-const readerStream = fs.createReadStream('input.txt.gz')
-.pipe(zlib.createGunzip())
-.pipe(fs.createWriteStream('input.txt'));
+    const name2 = 'demo3';
+    //name2 = 'demo3Changed';     // connot reassign value bcz of const keyword
 
-console.log('File decompressed');
+
+    console.log(name1);
+    console.log(name2);
+}
+
+//console.log(name + "ahjka");
+//console.log(name1 + "let")
+
+example();
+
+let name1 = "Can declared outside of blocked scope";
+console.log(name1);
+
+const name2 = "can be redeclare outside of blocked scope";
+console.log(name2);
