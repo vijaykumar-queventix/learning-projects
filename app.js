a2819cc8451614ad4bcc791fcfa07713d3eb066d
Vijay Kumar <vijay.kumar@quventix.com>
2021-02-13 12:53:15 +0530

    Feautes of ES6-Default and Rest parameters

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
-// Arrow fucntion 
+// Default parametes
 
-// old way of defing funciton
-function name(parameter1, parameter2){
-   let c = parameter1+ parameter2;
-   console.log(c);
-}
-name(5,10);
-
-
-// or
-
-var addtion = function(parameter1, parameter2){
-    let c = parameter1+ parameter2;
-    console.log(c);
- }
- addtion(5,46);
-
-
-
-// arrow function
+// Es5
 
 const Arrowfunction = (parameter1,parameter2)=>{
+    parameter2 = (typeof(parameter2) !== "undefined") ? parameter2 : 20 ;       // difind value of parameter2 is 20 if not defined on calling function
     let c = parameter1+ parameter2;
     console.log(c);
 }
 
-Arrowfunction(2, 4);
+Arrowfunction(2);
+
+// Es6
+
+const Defultfunction = (parameter1,parameter2=20)=>{            // difind value of parameter2 is 20 if not defined on calling function
+    let c = parameter1+ parameter2;
+    console.log(c);
+}
+
+Defultfunction(4);
+
+// rest parameters 
+
+const sum = (...inputs)=>{                      // converts the arguments in an array 
+    let total = 0;
+    for(let i of inputs){
+        total += i; 
+        //console.log(total);
+    }
+    console.log(inputs);
+    console.log(total);
+}
+
+sum(1,2,4,5,8,6);               // we can give infinte no. of arguments 
+sum(1,2,4,5,8,6,5,7,8,4,7);  
